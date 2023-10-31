ab03584bd2f45874b197a929e6586abb49505fea
Дмитрий <d77s@inbox.ru>
2023-10-31 22:34:10 +0300

    Next

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -10,8 +10,14 @@ router = routers.DefaultRouter()
 router.register('api/v1/posts', PostViewSet)
 
 #  Для взаимодействия с ресурсами опишите и настройте такие эндпоинты:
-#  api/v1/api-token-auth/ (POST): передаём логин и пароль, получаем токен.
-#  api/v1/posts/ (GET, POST): получаем список всех постов или создаём нов.пост.
+#  done api/v1/api-token-auth/ (POST): передаём логин и пароль, получаем токен.
+#  done api/v1/posts/ (GET, POST): получаем список всех постов или создаём нов.пост.
+#  только для своих постов, не чужих! (требуется проверка на авторство)
+#  !! не получается !!! api/v1/posts/{post_id}/ (GET, PUT, PATCH, DELETE): получаем, редактируем или удаляем пост по id.
+#  api/v1/groups/ (GET): получаем список всех групп.
+#  api/v1/groups/{group_id}/ (GET): получаем информацию о группе по id.
+#  api/v1/posts/{post_id}/comments/ (GET, POST): получаем список всех комментариев поста с id=post_id или создаём новый, указав id поста, который хотим прокомментировать.
+#  api/v1/posts/{post_id}/comments/{comment_id}/ (GET, PUT, PATCH, DELETE): получаем, редактируем или удаляем комментарий по id у поста с id=post_id.
 
 urlpatterns = [
     path('', include(router.urls)),
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,7 @@
 #  from django.shortcuts import render
 
 from rest_framework import viewsets
+# from rest_framework.permissions import
 
 from posts.models import Post  # Group, Comment
 from posts.serializers import PostSerializer
@@ -9,3 +10,9 @@ from posts.serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            pass
+            # raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
