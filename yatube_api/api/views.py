e3694357fb3181359fd6cc3f16ab0e73d16885c7
Дмитрий <d77s@inbox.ru>
2023-11-04 20:42:21 +0300

    Next

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
-#  from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+
 from posts.models import Post, Group, Comment
 from posts.serializers import (PostSerializer,
                                CommentSerializer,
@@ -15,22 +15,39 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
     def perform_create(self, serializer):
+        '''Перегружает метод вьюсета по созданию объекта.
+        При создании поста нам надо, чтобы
+        id автора камента брался именно из реквеста
+        (даже если какой-то и пришел в словаре api, мы ему не верим).
+        '''
         serializer.save(author=self.request.user)
 
     def perform_update(self, serializer):
+        '''Перегружает метод вьюсета по обновлению объекта,
+        хоть частичному хоть полному.
+        Вызывает родительский (неперегруженный) метод, только если
+        совпадает автор обновляемого объекта и залогиненный пользователь.
+        '''
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
         super(PostViewSet, self).perform_update(serializer)
 
     def perform_destroy(self, instance):
+        '''Перегружает метод вьюсета по удалению объекта.
+        Вызывает родительский (неперегруженный) метод, только если
+        совпадает автор удаляемого объекта и залогиненный пользователь.
+        '''
         if instance.author != self.request.user:
             raise PermissionDenied('Удаление чужого контента запрещено!')
         super(PostViewSet, self).perform_destroy(instance)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
-    # Наследовались специально от ReadOnly,
-    # чтобы через api нельзя было создать новую группу.
+    '''Вьюсет для групп постов.
+    Наследовались специально от ReadOnly,
+    чтобы через api нельзя было создать новую группу.
+    А только через админку можно было бы.
+    '''
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
@@ -39,24 +56,42 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
 
     def get_queryset(self):
-        '''Получает все комменты к одному конкретному посту.
+        '''Перегружает кверисет таким образом,
+        чтобы он содержал не вообще все каменты,
+        а только каменты к одному конкретному посту.
+        Номер поста через кварг получаем из урла.
         '''
+        print(self.kwargs)
         post_id = self.kwargs.get("post_id")
         post = get_object_or_404(Post, id=post_id)
-        new_queryset = Comment.objects.filter(post=post)
-        return new_queryset
+        return Comment.objects.filter(post=post)
 
     def perform_create(self, serializer):
-        post_id = self.kwargs.get("post_id")
-        post = get_object_or_404(Post, id=post_id)
+        '''Перегружает метод вьюсета по созданию объекта.
+        При создании камента нам надо, чтобы:
+        - id автора камента брался именно из реквеста
+        (даже если какой-то и пришел в словаре api, мы ему не верим);
+        - номер пост, к которому создаем камент, берем из урла по регулярке и
+        вытаскиваем сей объект поста из базы постов.
+        '''
+        post = get_object_or_404(Post, id=self.kwargs.get("post_id"))
         serializer.save(author=self.request.user, post=post)
 
     def perform_update(self, serializer):
+        '''Перегружает метод вьюсета по обновлению объекта,
+        хоть частичному хоть полному.
+        Вызывает родительский (неперегруженный) метод, только если
+        совпадает автор обновляемого объекта и залогиненный пользователь.
+        '''
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
         super(CommentViewSet, self).perform_update(serializer)
 
     def perform_destroy(self, instance):
+        '''Перегружает метод вьюсета по удалению объекта.
+        Вызывает родительский (неперегруженный) метод, только если
+        совпадает автор удаляемого объекта и залогиненный пользователь.
+        '''
         if instance.author != self.request.user:
             raise PermissionDenied('Удаление чужого контента запрещено!')
         super(CommentViewSet, self).perform_destroy(instance)
--- yatube_api/posts/serializers.py
+++ yatube_api/posts/serializers.py
@@ -4,11 +4,26 @@ from rest_framework import serializers
 
 
 class PostSerializer(serializers.ModelSerializer):
+    '''Класс сериализатора постов.'''
+    # В поле автора хотим отдавать не id, а имя автора, поэтому
+    # слегка переопределим поле author.
+    # А по получении, хотим его игнорировать (read_only=True)
+    # и брать из реквеста.
     author = serializers.StringRelatedField(many=False, read_only=True)
 
     class Meta:
         model = Post
-        fields = 'id', 'text', 'pub_date', 'author', 'image', 'group', 'comments'
+        # Перечень сериализируемых полей.
+        # Можно было бы и просто __all__,
+        # но нам надо добавить related поле comments.
+        fields = ('id',
+                  'text',
+                  'pub_date',
+                  'author',
+                  'image',
+                  'group',
+                  'comments',
+                  )
         read_only_fields = (
             'author',
             'group',
@@ -16,6 +31,11 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    '''Класс сериализатора каментов к постам.'''
+    # В поле автора хотим отдавать не id, а имя автора, поэтому
+    # слегка переопределим поле author.
+    # А по получении, хотим его игнорировать (read_only=True)
+    # и брать из реквеста.
     author = serializers.StringRelatedField(read_only=True)
 
     class Meta:
@@ -25,6 +45,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class GroupSerializer(serializers.ModelSerializer):
+    '''Класс сериализатора групп постов.'''
     class Meta:
         model = Group
         fields = '__all__'
