f52e9992d4a5a2a39adcc4fd2761df31b1b87cb1
Дмитрий <d77s@inbox.ru>
2023-11-05 20:15:34 +0300

    Next

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,10 @@
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
-from rest_framework.exceptions import PermissionDenied
 
 from .permissions import IsOwnerOrReadOnly
+from .serializers import (PostSerializer, GroupSerializer,
+                          CommentSerializer)
 from posts.models import Post, Group, Comment
-from posts.serializers import (PostSerializer, GroupSerializer,
-                               CommentSerializer)
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -20,7 +19,13 @@ class PostViewSet(viewsets.ModelViewSet):
         (даже если какой-то и пришел в словаре api, мы ему не верим).
         '''
         serializer.save(author=self.request.user)
-
+    #
+    # Пока не буду стирать старый способ аутентификации,
+    # (путем перегрузки методов класса),
+    # хотя он уже и не используются, но я хочу сохранить их в коде
+    # для помощи самому себе в будущем.
+    # Стереть всегда успею перед релизом.
+    #
     # def perform_update(self, serializer):
     #     '''Перегружает метод вьюсета по обновлению объекта,
     #     хоть частичному хоть полному.
@@ -54,6 +59,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     '''Вьюсет для каментов.'''
     serializer_class = CommentSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
 
     def get_queryset(self):
         '''Перегружает кверисет таким образом,
@@ -61,7 +67,6 @@ class CommentViewSet(viewsets.ModelViewSet):
         а только каменты к одному конкретному посту.
         Номер поста через кварг получаем из урла.
         '''
-        print(self.kwargs)
         post_id = self.kwargs.get("post_id")
         post = get_object_or_404(Post, id=post_id)
         return Comment.objects.filter(post=post)
@@ -76,22 +81,28 @@ class CommentViewSet(viewsets.ModelViewSet):
         '''
         post = get_object_or_404(Post, id=self.kwargs.get("post_id"))
         serializer.save(author=self.request.user, post=post)
+    #
+    # Пока не буду стирать старый способ аутентификации,
+    # (путем перегрузки методов класса),
+    # хотя он уже и не используются, но я хочу сохранить их в коде
+    # для помощи самому себе в будущем.
+    # Стереть всегда успею перед релизом.
+    #
+    # def perform_update(self, serializer):
+    #     '''Перегружает метод вьюсета по обновлению объекта,
+    #     хоть частичному хоть полному.
+    #     Вызывает родительский (неперегруженный) метод, только если
+    #     совпадает автор обновляемого объекта и залогиненный пользователь.
+    #     '''
+    #     if serializer.instance.author != self.request.user:
+    #         raise PermissionDenied('Изменение чужого контента запрещено!')
+    #     super(CommentViewSet, self).perform_update(serializer)
 
-    def perform_update(self, serializer):
-        '''Перегружает метод вьюсета по обновлению объекта,
-        хоть частичному хоть полному.
-        Вызывает родительский (неперегруженный) метод, только если
-        совпадает автор обновляемого объекта и залогиненный пользователь.
-        '''
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        '''Перегружает метод вьюсета по удалению объекта.
-        Вызывает родительский (неперегруженный) метод, только если
-        совпадает автор удаляемого объекта и залогиненный пользователь.
-        '''
-        if instance.author != self.request.user:
-            raise PermissionDenied('Удаление чужого контента запрещено!')
-        super(CommentViewSet, self).perform_destroy(instance)
+    # def perform_destroy(self, instance):
+    #     '''Перегружает метод вьюсета по удалению объекта.
+    #     Вызывает родительский (неперегруженный) метод, только если
+    #     совпадает автор удаляемого объекта и залогиненный пользователь.
+    #     '''
+    #     if instance.author != self.request.user:
+    #         raise PermissionDenied('Удаление чужого контента запрещено!')
+    #     super(CommentViewSet, self).perform_destroy(instance)
--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -4,6 +4,8 @@ from .models import Comment, Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
+    # Ревьюер отметил, что "отличная работа",
+    # но это не я писал. Это приложение мне дали готовым.
     list_display = ('pk', 'text', 'pub_date', 'author')
     search_fields = ('text',)
     list_filter = ('pub_date',)
